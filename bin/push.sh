4bada20c01d3fb6b80e9557e66613fc5382eb9f3
Michael Olie <mike@grolens.com>
2022-01-25 07:14:16 -0800

    ci: removed git commit hooks question from push script

--- bin/push.sh
+++ bin/push.sh
@@ -3,14 +3,7 @@ git add .
 git add -u
 read -r -p 'Commit message: ' desc  # prompt user for commit message
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-read -p "Do you wish to run git commit hooks? (y/n)? " answer
-case ${answer:0:1} in
-    y|Y )
-        git commit -m "$desc"
-    ;;
-    * )
-        git commit -m "$desc" -n 
-    ;;
+git commit -m "$desc"
 esac
 git push origin $branch
 

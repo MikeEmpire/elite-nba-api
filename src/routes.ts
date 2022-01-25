b2281c38e773bcb0f24cc149394e56ad51a2b166
Michael Olie <mike@grolens.com>
2022-01-24 20:19:37 -0800

    added middleware for errors

--- src/routes.ts
+++ src/routes.ts
@@ -1,31 +1,13 @@
+import { Express } from "express";
+
 import players from "../routes/players";
-import {
-  Express,
-  Request,
-  ErrorRequestHandler,
-  NextFunction,
-  Response,
-} from "express";
+
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 export default (app: Express) => {
   app.use("/api/players", players);
 
-  app.use(
-    (
-      err: ErrorRequestHandler,
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ) => {
-      let statusCode: number = 500;
-      if (!err) {
-        // eslint-disable-next-line no-param-reassign
-        statusCode = 400;
-      }
-      // TODO: Enhance error handling
-      // console.log(err);
-
-      return res.status(statusCode).send(err);
-    }
-  );
+  app.use(errorHandler);
+  app.use(notFoundHandler);
 };

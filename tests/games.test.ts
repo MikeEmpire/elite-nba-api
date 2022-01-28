99f28408351df9890881517277de1c3ee542222f
Michael Olie <mike@grolens.com>
2022-01-28 09:06:08 -0800

    chore: updated test info

--- tests/games.test.ts
+++ tests/games.test.ts
@@ -1,8 +1,8 @@
 import app from "../src";
 import request from "supertest";
 
-describe("Able to get necessary data from API", () => {
-  return it("Able to get Lebron's season stats", async (): Promise<void> => {
+describe("Able to get information about games today", () => {
+  return it("Able to get today's games and info", async (): Promise<void> => {
     const PATH: string = `/api/games`;
     const result = await request(app).get(PATH).send();
     expect(result.status).toBe(200);

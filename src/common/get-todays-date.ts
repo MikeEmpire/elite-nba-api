18edb48833d55c6280d1632f806015d442f7deee
Michael Olie <mike@grolens.com>
2022-03-04 23:24:41 -0800

    feat: able to get date from game day schedule

--- src/common/get-todays-date.ts
+++ src/common/get-todays-date.ts
@@ -1,5 +1,10 @@
-export default (): string => {
-  const today = new Date();
+export default (dateToUse?: string): string => {
+  let today;
+  if (dateToUse) {
+    today = new Date(dateToUse);
+  } else {
+    today = new Date();
+  }
   const dd = String(today.getDate()).padStart(2, "0");
   const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
   const yyyy = today.getFullYear();
--- src/services/games_service.ts
+++ src/services/games_service.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
-import { Response, NextFunction } from "express";
+import { Response, Request, NextFunction } from "express";
 
 import getTodaysDate from "../common/get-todays-date";
 import IGAME_DATA from "../interfaces/gameinfo.interface";
 
 export const getTodaysGames = async (
-  _: any,
+  req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    const todaysDate = await getTodaysDate();
+    const queryDate: string = req.query.date as string;
+    const todaysDate = getTodaysDate(queryDate);
     const url = `https://data.nba.net/10s/prod/v1/${todaysDate}/scoreboard.json`;
     const { data }: { data: IGAME_DATA } = await axios.get(url);
     return res.status(200).send({ data, message: "Hit!" });
